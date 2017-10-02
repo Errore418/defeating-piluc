b2b3cec7b928d6a4df94385a236c4193958e847f
Claudio Nave <c.nave@promonow.it>
2017-10-02 19:18:42 +0200

    Versione base

---
+++ src/gj/quoridor/player/avversario/AvversarioPlayer.java
@@ -0,0 +1,20 @@
+package gj.quoridor.player.avversario;
+
+import gj.quoridor.player.Player;
+
+public class AvversarioPlayer implements Player{
+
+	@Override
+	public int[] move() {
+		return new int[] {0,0};
+	}
+
+	@Override
+	public void start(boolean arg0) {
+	}
+
+	@Override
+	public void tellMove(int[] arg0) {
+	}
+
+}
