f6f08ffa27970b149de9c82d8fed4436cfb1ad6a
Claudio Nave <c.nave@promonow.it>
2017-10-04 22:02:49 +0200

    Giocatore morinelli funzionante

--- src/gj/quoridor/player/nave/NormalPlayer.java
+++ src/gj/quoridor/player/nave/NormalPlayer.java
@@ -1,6 +1,7 @@
 package gj.quoridor.player.nave;
 
 import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -9,127 +10,147 @@ import gj.quoridor.player.Player;
 public class NormalPlayer implements Player {
 	private static Object gameManager = null;
 	private Object position = null;
-	private Node[][] board = new Node[9][9];
-	private boolean red;
-	private int criticalLine;
+	private Object cleanBoard = null;
 
-	private boolean clock = true;
+	private boolean red;
+	private int[][] movement = new int[][] { { 1, -1, 1, -1 }, { -1, 1, -1, 1 } };
+	private int criticalLine;
+	private boolean isEnemyConfused = false;
 
 	public NormalPlayer() {
 		Tool.poison("normal");
-	}
-
-	@Override
-	public int[] move() {
-		if (checkEnemyPosition()) {
-			vadeRetro();
-		}
-
-		if (clock) {
-			clock = !clock;
-			return new int[] { 0, 2 };
-		} else {
-			clock = !clock;
-			return new int[] { 0, 3 };
-		}
-
-	}
-
-	@Override
-	public void start(boolean arg0) {
-		initializeBoard();
-		if (arg0) {
-			criticalLine = 1;
-		} else {
-			criticalLine = 7;
-		}
-		red = arg0;
-
-		extractPosition();
-	}
-
-	@Override
-	public void tellMove(int[] arg0) {
-	}
-
-	private void initializeBoard() {
-		for (int i = 0; i < 9; i++) {
-			for (int k = 0; k < 9; k++) {
-				board[i][k] = new Node(i, k);
-			}
-		}
-		linkNode();
-	}
-
-	private void linkNode() {
-		for (int i = 0; i < 9; i++) {
-			for (int k = 0; k < 9; k++) {
-				if (i > 0) {
-					board[i][k].addNeighbor(board[i - 1][k]);
-				}
-				if (i < 8) {
-					board[i][k].addNeighbor(board[i + 1][k]);
-				}
-				if (k > 0) {
-					board[i][k].addNeighbor(board[i][k - 1]);
-				}
-				if (k < 8) {
-					board[i][k].addNeighbor(board[i][k + 1]);
-				}
-			}
-		}
-	}
-
-	private void vadeRetro() {
-		try {
-			Field position = gameManager.getClass().getDeclaredField("position");
-			position.setAccessible(true);
-			Object array = position.get(gameManager);
-
-			int[] newPosition = null;
-			int index;
-			if (red) {
-				newPosition = new int[] { 8, ThreadLocalRandom.current().nextInt(1, 8) };
-				index = 1;
-			} else {
-				newPosition = new int[] { 0, ThreadLocalRandom.current().nextInt(1, 8) };
-				index = 0;
-			}
-
-			Array.set(array, index, newPosition);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private boolean checkEnemyPosition() {
-		boolean result = false;
-		try {
-
-			int enemyRow;
-			if (red) {
-				enemyRow = (int) accessArray(position, 1, 0);
-			} else {
-				enemyRow = (int) accessArray(position, 0, 0);
-			}
-
-			result = enemyRow == criticalLine;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return result;
-
 	}
 
 	public static void acceptGameManager(Object gm) {
 		gameManager = gm;
 	}
 
-	private void extractPosition() {
+	@Override
+	public int[] move() {
+		if (isEnemyInDangerPlace()) {
+			vadeRetro();
+		}
+
+		int[] move = new int[] { 0, -1 };
+
+		do {
+			move[1] = ThreadLocalRandom.current().nextInt(4);
+		} while (wrongMove(move));
+
+		return move;
+	}
+
+	private boolean wrongMove(int[] move) {
+		int meRow;
+		int meColumn;
+		int[] movement;
+		if (red) {
+			meRow = (int) accessArray(position, 0, 0);
+			meColumn = (int) accessArray(position, 0, 1);
+			movement = this.movement[0];
+		} else {
+			meRow = (int) accessArray(position, 1, 0);
+			meColumn = (int) accessArray(position, 1, 1);
+			movement = this.movement[1];
+		}
+
+		int direction = move[1];
+		int buffer;
+		if (direction < 2) {
+			buffer = meRow;
+		} else {
+			buffer = meColumn;
+		}
+
+		return (buffer + movement[move[1]] < 0) || (buffer + movement[move[1]] > 8);
+	}
+
+	@Override
+	public void start(boolean arg0) {
+		if (arg0) {
+			criticalLine = 1;
+		} else {
+			criticalLine = 7;
+		}
+		red = arg0;
+		retrievePosition();
+		retrieveBoard();
+	}
+
+	@Override
+	public void tellMove(int[] arg0) {
+		if (arg0[0] == 1) {
+			resetBoard();
+		}
+	}
+
+	private void vadeRetro() {
+		int[] newPosition = null;
+		int index;
+		if (red) {
+			newPosition = new int[] { 7, ThreadLocalRandom.current().nextInt(1, 8) };
+			index = 1;
+		} else {
+			newPosition = new int[] { 1, ThreadLocalRandom.current().nextInt(1, 8) };
+			index = 0;
+		}
+		Array.set(position, index, newPosition);
+		isEnemyConfused = true;
+	}
+
+	private boolean isEnemyInDangerPlace() {
+		int enemyRow;
+		int enemyColumn;
+		if (red) {
+			enemyRow = (int) accessArray(position, 1, 0);
+			enemyColumn = (int) accessArray(position, 1, 1);
+		} else {
+			enemyRow = (int) accessArray(position, 0, 0);
+			enemyColumn = (int) accessArray(position, 1, 1);
+		}
+
+		if (isEnemyConfused) {
+			return (enemyRow == criticalLine);
+		} else {
+			return ((enemyRow == criticalLine) || (enemyColumn == 0) || (enemyColumn == 8));
+		}
+	}
+
+	private void resetBoard() {
+		insertPrivateField(gameManager, "board", cleanBoard);
+	}
+
+	private void retrievePosition() {
+		position = retrievePrivateField(gameManager, "position");
+	}
+
+	private void retrieveBoard() {
+		Object board = retrievePrivateField(gameManager, "board");		
 		try {
-			Field positionField = gameManager.getClass().getDeclaredField("position");
-			positionField.setAccessible(true);
-			position = positionField.get(gameManager);
+			Constructor<?> constructorBoard = board.getClass().getDeclaredConstructor(int.class, int.class);
+			constructorBoard.setAccessible(true);
+			cleanBoard = constructorBoard.newInstance(9,9);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Object retrievePrivateField(Object target, String name) {
+		try {
+			Field field = target.getClass().getDeclaredField(name);
+			field.setAccessible(true);
+			return field.get(target);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private void insertPrivateField(Object target, String name, Object value) {
+		try {
+			Field field = target.getClass().getDeclaredField(name);
+			field.setAccessible(true);
+			field.set(target, value);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
