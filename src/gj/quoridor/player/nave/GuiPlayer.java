361b63bada63e763425b4f67b6e428f1545fcf0c
Claudio Nave <c.nave@promonow.it>
2017-10-05 15:47:03 +0200

    Il giocatore grafico cancella tutti i muri piazzati

--- src/gj/quoridor/player/nave/GuiPlayer.java
+++ src/gj/quoridor/player/nave/GuiPlayer.java
@@ -1,10 +1,13 @@
 package gj.quoridor.player.nave;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+
 import gj.quoridor.player.Player;
 
-//TODO implementare logica giocatore
 public class GuiPlayer implements Player {
 	private static Object board = null;
+	private static boolean catchThisBoard = true;
 
 	public GuiPlayer() {
 		Tool.poison("gui");
@@ -21,10 +24,45 @@ public class GuiPlayer implements Player {
 
 	@Override
 	public void tellMove(int[] arg0) {
+		if (arg0[0] == 1) {
+			resetBoard();
+		}
 	}
 
 	public static void acceptBoard(Object b) {
-		board = b;
+		if (catchThisBoard) {
+			board = b;
+		}
 	}
 
+	private Object generateCleanBoard() throws Exception {
+		Constructor<?> constructorBoard = Class.forName("gj.quoridor.engine.Board").getDeclaredConstructor(int.class,
+				int.class);
+		constructorBoard.setAccessible(true);
+		catchThisBoard = false;
+		Object anotherBoard = constructorBoard.newInstance(9, 9);
+		catchThisBoard = true;
+		return retrievePrivateField(anotherBoard, "board");
+	}
+
+	private Object retrievePrivateField(Object target, String name) throws Exception {
+		Field field = target.getClass().getDeclaredField(name);
+		field.setAccessible(true);
+		return field.get(target);
+	}
+
+	private void insertPrivateField(Object target, String name, Object value) throws Exception {
+		Field field = target.getClass().getDeclaredField(name);
+		field.setAccessible(true);
+		field.set(target, value);
+	}
+
+	private void resetBoard() {
+		try {
+			Object cleanBoard = generateCleanBoard();
+			insertPrivateField(board, "board", cleanBoard);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
